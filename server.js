d8003fa996b2382c9f59c9b229c73437634356c7
gayathriT <98060548+gayathriTGit@users.noreply.github.com>
2025-08-28 18:52:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const students = require('./StudentRoutes');
 const app = express();
 const PORT = process.env.PORT || 9005;
 
-// remove the fingerprint:
-app.disable('x-powered-by');
-
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const spec = YAML.load(path.join(__dirname, 'openapi.yaml'));
@@ -19,8 +16,11 @@ app.get('/swagger.json', (_, res) => res.json(spec));
 // Middleware to parse JSON requests
 app.use(express.json());
 
+
 //health
 app.get('/health', (_,res)=>res.json({ok:true}));
+
+app.get('/', (req, res) => res.json({ message: 'welcome to Students API' }));
 
 //routes
 app.use('/students', students);
@@ -29,10 +29,11 @@ app.use('/students', students);
 app.use((req,res)=> res.status(404).json({error: 'route not found'}));
 
 //error handler
-app.use((err, req, res) =>{
-    console.error(err);
-    res.status(500).json({error: 'server error'});
-})
+app.use((err, req, res, next) => {
+  console.error(err);
+  if (res.headersSent) return next(err);
+  res.status(500).json({ error: 'server error' });
+});
 
 // Start the server
 app.listen(PORT, () => {
